9a8f6f82dd09da432826749fbfe3a737b5f5047a
Abhaskar1 <akshaybhaskar26@gmail.com>
2020-05-20 19:19:28 +0530

    Added Class Based Components

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData=[
+    {
+        id:1,
+        text:"Take out the trash",
+        completed:true
+    },
+    {
+        id:2,
+        text:"Grocery shopping",
+        completed:false
+
+    },
+    {
+        id:3,
+        text:"Mow lawn",
+        completed:true
+    },
+    {
+        id:4,
+        text:"Clean getho tank",
+        completed:false
+    },
+    {
+        id:5,
+        text:"Play",
+        completed:false
+    }
+]
+export default todosData
